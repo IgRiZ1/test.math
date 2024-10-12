15274bc1caf8d00c9692835510a0913cc28c9035
IgRiZ1 <dalil.blh@outlook.be>
2024-10-12 11:02:21 +0200

    Update testmath.mjs

--- testmath.mjs
+++ testmath.mjs
@@ -1,7 +1,21 @@
-function random(min, max) {
+
+
+
+
+
+
+function random(min, max , raden) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+    
+    if (raden == num){
+        console.log('juist getal geraden'); 
+         
+    } else {
+        console.log('fout getal');
+    }
+return num ;
+  
   }
   
-  console.log(random(1,10));
+  console.log(random(1,10,4));
   
